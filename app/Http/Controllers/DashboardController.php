ce3a68fd71b21d05ca6581ef25c16af8be0fe85b
anharsaja <anharmuk12@gmail.com>
2025-11-04 17:41:51 +0700

    :bug: fix: fixed db

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\BasisPengetahuan;
+use App\Models\BobotGejala;
 use Illuminate\Routing\Controller;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $basis = BasisPengetahuan::with(['penyakit', 'gejala'])->get();
+        $basis = BobotGejala::with(['penyakit', 'gejala'])->get();
         return view('pages.dashboard.index');
     }
 }
